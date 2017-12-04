94cb4d7d2cd9eb9f6b68be6e7133a32de458f60e
Faisal Khan <faisalkhan1690@gmail.com>
2017-12-04 19:33:40 +0530

    init commit

---
+++ android/app/src/main/java/com/awesome/FirstActivity.java
@@ -0,0 +1,19 @@
+package com.awesome;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class FirstActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void startReactCode(View view){
+        startActivity(new Intent(FirstActivity.this,MainActivity.class));
+    }
+}
